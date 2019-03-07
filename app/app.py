f60d14fd2b80ad3d50cda947e1248fbc9a78b721
Luke Lee <llee@ltgc.com>
2019-03-07 12:24:26 -0700

    remove todo

--- app/app.py
+++ app/app.py
@@ -10,7 +10,6 @@ db = None
 def create_app(config=None):
     app = Flask(__name__)
 
-    #TODO add way to specify configuration, probably a script to run after sourcing env.sh
     config = config or DebugConfig
     app.register_blueprint(calls)
     app.config.from_object(config)
