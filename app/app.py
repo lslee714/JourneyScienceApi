c8fab6bfde1127fd6d8e5379fe57ceeeed97eeff
Luke Lee <lslee714@gmail.com>
2019-03-06 19:50:36 -0700

    More env variables, load in configuration for flask

--- app/app.py
+++ app/app.py
@@ -1,8 +1,14 @@
 from flask import Flask
 
 from .blueprints import calls
+from .config import DebugConfig
 
-def create_app():
+
+def create_app(config=None):
     app = Flask(__name__)
+
+    #TODO add way to specify configuration, probably a script to run after sourcing env.sh
+    config = config or DebugConfig
     app.register_blueprint(calls)
+    app.config.from_object(config)
     return app
