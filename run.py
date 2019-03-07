aa508830357c5db019872b03d00b9767e7472cbc
Luke Lee <llee@ltgc.com>
2019-03-07 12:24:13 -0700

    refactor run to use arg parser to get the config type

--- run.py
+++ run.py
@@ -1,5 +1,29 @@
 from app import create_app
+from configs import Config, ProductionConfig, DebugConfig
+
+def get_config(option):
+    """ Return the config """
+    configMapping = {
+        'debug': DebugConfig,
+        'live': ProductionConfig
+    }
+    config = configMapping.get(option)
+    if not config:
+        config = Config
+
+    return config
+
 
 if __name__ == '__main__':
-    app = create_app()
+    import argparse
+
+    argParser = argparse.ArgumentParser(description='Run the Journey Science API')
+
+    argParser.add_argument('--config', dest='config', nargs='?', choices=('debug', 'live'), default=None,
+                           help='The type of configuration to run the service in')
+
+    args = argParser.parse_args()
+
+    appConfig = get_config(args.config)
+    app = create_app(config=appConfig)
     app.run()
