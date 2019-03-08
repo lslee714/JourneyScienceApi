5c950a2bbda57ab0e2465d43acc719d1a2102a6b
Luke Lee <lslee714@gmail.com>
2019-03-07 22:29:11 -0700

    path helper to get path and filename

--- calls/tests/test_path_manager.py
+++ calls/tests/test_path_manager.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from unittest import TestCase
 from unittest.mock import Mock
 
@@ -6,19 +7,59 @@ from models import Upload
 from ..upload_path_manager import UploadPathManager
 
 
+TEST_BASE_PATH = '/test/path'
+TEST_EXTENSION = 'test'
+
 class test_get_path(TestCase):
     """Test cases for the get_path method"""
 
     def test_with_non_upload(self):
         """Attempting to get a path with a non-Upload record should raise an error"""
-        testCaseObject = UploadPathManager()
+        pathManager = UploadPathManager(TEST_BASE_PATH)
         nonUploadRecord = Mock()
         with self.assertRaises(ValueError):
-            result = testCaseObject.get_path(nonUploadRecord)
+            result = pathManager.get_path(nonUploadRecord)
 
     def test_without_id(self):
         """Attempting to get a path without an ID should raise an error"""
-        testCaseObject = UploadPathManager()
+        pathManager = UploadPathManager(TEST_BASE_PATH)
         uploadWithNoId = Upload()
         with self.assertRaises(ValueError):
-            result = testCaseObject.get_path(uploadWithNoId)
+            result = pathManager.get_path(uploadWithNoId)
+
+    def test_with_valid_upload(self):
+        """A valid upload should return the expected path"""
+        pathManager = UploadPathManager(TEST_BASE_PATH)
+        uploadedTs = datetime.now()
+        validUpload = Upload(id=1, ts_uploaded=uploadedTs)
+
+        EXPECTED_RESULT = f"{TEST_BASE_PATH}/{uploadedTs.year}/{uploadedTs.month}/{uploadedTs.day}"
+        result = pathManager.get_path(validUpload)
+
+        self.assertEqual(result, EXPECTED_RESULT)
+
+
+class test_get_filename(TestCase):
+    """Test cases for the get_filename static method"""
+
+    def test_with_non_upload(self):
+        """Cannot return a filename for a non Upload record"""
+        nonUploadRecord = Mock()
+        with self.assertRaises(ValueError):
+            result = UploadPathManager.get_filename(nonUploadRecord, TEST_EXTENSION)
+
+    def test_without_id(self):
+        """Attempting to get a filename without a ts should raise an error"""
+        uploadWithNoTs = Upload()
+        with self.assertRaises(ValueError):
+            result = UploadPathManager.get_filename(uploadWithNoTs, TEST_EXTENSION)
+
+    def test_valid_upload(self):
+        """Given a valid upload record, should return the expected filename"""
+        testUploadTs = datetime.now()
+        validUpload = Upload(id=1, ts_uploaded=testUploadTs)
+
+        expectedResult = f"{testUploadTs.strftime(UploadPathManager.FILENAME_FORMAT)}_{validUpload.id}.{TEST_EXTENSION}"
+
+        result = UploadPathManager.get_filename(validUpload, TEST_EXTENSION)
+        self.assertEqual(result, expectedResult)
--- calls/upload_path_manager.py
+++ calls/upload_path_manager.py
@@ -1,12 +1,31 @@
+from pathlib import Path
+
 from models import Upload
 
 class UploadPathManager:
     """Manages path functionality for call uploads"""
+    FILENAME_FORMAT = '%I_%M_%P'
+
+    def __init__(self, basePath):
+        self.basePath = Path(basePath)
 
     def get_path(self, upload):
         """Return the path for an upload record"""
         if not isinstance(upload, Upload):
             raise ValueError("Can only get path for Upload records")
 
-        if not upload.id:
+        if not upload.id: #checking id also ensures it has a ts
             raise ValueError("Upload record not yet flushed or committed, cannot get path")
+        uploadedTs = upload.ts_uploaded
+        pathParts = map(str, [uploadedTs.year, uploadedTs.month, uploadedTs.day])
+        path = self.basePath.joinpath('/'.join(pathParts))
+        return str(path)
+    @staticmethod
+    def get_filename(upload, extension):
+        """Return the name for an upload"""
+        if not isinstance(upload, Upload):
+            raise ValueError("Can only create a filename for Upload records")
+
+        if not upload.ts_uploaded:
+            raise ValueError("Upload record not yet flushed or committed, cannot get filename")
+        return upload.ts_uploaded.strftime(f"{UploadPathManager.FILENAME_FORMAT}_{upload.id}.{extension}")
