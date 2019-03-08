610ed00076696955575be6703b693c209d94d931
Luke Lee <lslee714@gmail.com>
2019-03-07 21:48:55 -0700

    use get instead

--- configs/base.py
+++ configs/base.py
@@ -2,4 +2,4 @@ import os
 
 class Config:
     """Base configuration settings for flask"""
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URI'] or 'sqlite:///:memory'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI', 'sqlite:///:memory')
