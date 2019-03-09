0ea76d69b4d73d4afdc5722090fc3fa5cc071add
Luke Lee <lslee714@gmail.com>
2019-03-09 12:29:20 -0700

    forgot one file for simplificatoin

--- app/static/calls/upload_service.js
+++ app/static/calls/upload_service.js
@@ -1,10 +1,8 @@
 "use strict";
-(function(a, $){
-    a.module("calls")
-        .service('UploadApiService', ['$http', function($http){
-            this.post = function(postData, options){
-                var options = options || {};
-                return $http.post('', postData, options);
-            };
-        }])
-})(angular, jQuery)
+a.module("calls")
+    .service('UploadApiService', ['$http', function($http){
+        this.post = function(postData, options){
+            var options = options || {};
+            return $http.post('', postData, options);
+        };
+    }])
