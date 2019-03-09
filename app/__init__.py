d7446b89fceb435f1b8bc617aefa08cb20ffbffc
Luke Lee <lslee714@gmail.com>
2019-03-08 21:59:12 -0700

    dont default memory sqlitei nstance as it wont have models, dont default config since the arg parser already does that

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,22 @@
-from .app import create_app
+from flask import Flask, g
+from flask_sqlalchemy import SQLAlchemy
+
+db = None
+
+def create_app(config):
+    app = Flask(__name__)
+    app.config.from_object(config)
+    global db
+
+    db = SQLAlchemy(app)
+    register_blueprints(app)
+    return app
+
+def register_blueprints(app):
+    """Register the blueprints"""
+    #have to import to get order proper for db to be initialized :(
+    from .blueprints import calls, index
+
+    blueprints = [calls, index]
+    for bp in blueprints:
+        app.register_blueprint(bp)
