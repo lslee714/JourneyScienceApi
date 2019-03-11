c3d2d3127db3a43b406cc96b6d8d4576e5a5049a
Luke Lee <lslee714@gmail.com>
2019-03-10 22:11:44 -0600

    angular template cache

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,24 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
 var minify = require('gulp-minify');
- 
+var templateCache = require('gulp-angular-templatecache');
+
+
+gulp.task('utilsTemplates', function (done) {
+  return gulp.src('app/static/utils/**/*.html')
+    .pipe(templateCache({module: 'utils'}))
+    .pipe(gulp.dest('app/static/utils'));
+    done()
+});
+
+gulp.task('callsTemplates', function (done) {
+  return gulp.src('app/static/calls/**/*.html')
+    .pipe(templateCache({module: 'calls'}))
+    .pipe(gulp.dest('app/static/calls'));
+    done()
+});
+
+
 gulp.task('concat', function(done) {
   return gulp.src([
             'app/static/utils/**/*.js',
@@ -24,4 +41,5 @@ gulp.task('minify', function(done) {
      done()
 });
 
-gulp.task('default', gulp.series('concat', 'minify'))
+
+gulp.task('default', gulp.series('utilsTemplates', 'callsTemplates', 'concat', 'minify'))
