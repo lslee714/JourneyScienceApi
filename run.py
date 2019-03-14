2bb1f1b9c6c1b55389c56e5f27249de5b4d5d542
Luke Lee <lslee714@gmail.com>
2019-03-13 18:03:18 -0600

    ADd additional command line args for run

--- run.py
+++ run.py
@@ -10,8 +10,17 @@ if __name__ == '__main__':
     argParser.add_argument('--config', dest='config', nargs='?', choices=('debug', 'live'), default=None,
                            help='The type of configuration to run the service in')
 
+    argParser.add_argument('--host', dest='host', nargs='?', default='localhost',
+                           help='The host IP to run the service in')
+
+    argParser.add_argument('--port', dest='port', nargs='?', default='',
+                           help='The port to run the service in')
+
+
     args = argParser.parse_args()
 
     appConfig = get_config(args.config)
+    host = args.host
+    port = args.port
     app = create_app(config=appConfig)
-    app.run()
+    app.run(host=host, port=port)
